b5c13f686f581ab8205bc6afa76ef08d234f3899
Petr Fadeev <p.fadeev@pbfgroup.ru>
2019-12-16 18:37:14 +0300

    v0.1.0 release

--- resp_gen.py
+++ resp_gen.py
@@ -3,17 +3,24 @@ from konfig import Config
 from time import sleep
 import datetime
 import logging
+import socket
 import sys
 import os
 import re
 
 
-def form_answer(request):
+def form_answer(request, rc_dict, status_ready=True):
     def gen_check_val(data):
         lrc = 0
         for symbol in data:
             lrc ^= symbol
         return chr(lrc)
+
+    def gen_trans_id():
+        time = datetime.datetime.now()
+        time = str(time.time()).split(':')
+        trans_id = time[0] + time[1] + ''.join(time[2].split('.'))[:4]
+        return trans_id
 
     def sale_qr_answer():
         pattern = re.compile(r'\x02.{45}')
@@ -22,11 +29,10 @@ def form_answer(request):
         RC = '001'
         card_type = 'RD'
         qr_code = 'g<QR:https://www.youtube.com/watch?v=dQw4w9WgXcQ>'
-        rrn = ''.join([str(randint(0, 9)) for i in range(8)])
+        rrn = gen_trans_id()
         const_fields = re.search(pattern, request)[0]
         approval_code = 'F' + ''.join([str(randint(0, 9)) for i in range(6)]) + ' A'
         optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
-
         try:
             sequence_number = re.search(seq_pattern, request)[0] + '0'
         except TypeError:
@@ -40,14 +46,22 @@ def form_answer(request):
 
     def sale_status_answer():
         pattern = re.compile(r'\x02.{45}')
-        rrn_pattern = re.compile(r'T(\d{8})')
         seq_pattern = re.compile(r'h\d{9}')
 
-        RC = '001'
+        if status_ready:
+            if rc_dict['sale_rc']:
+                RC = '001'
+                alipay_status = "g<ALIPAY_STATUS:SUCCESS>"
+            else:
+                RC = '602'
+                alipay_status = "g<ALIPAY_STATUS:FAILED>"
+        else:
+            RC = '601'
+            alipay_status = "g<ALIPAY_STATUS:UNKNOW>"
+
         card_type = 'RD'
-        rrn = re.search(rrn_pattern, request).group(1)
+        rrn = gen_trans_id()
         optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
-        alipay_status = "g<ALIPAY_STATUS:SUCCESS>"
         const_fields = re.search(pattern, request)[0]
         sequence_number = re.search(seq_pattern, request)[0] + '0'
 
@@ -59,11 +73,22 @@ def form_answer(request):
 
     def sale_final_answer():
         pattern = re.compile(r'\x02.{45}')
+        seq_pattern = re.compile(r'h\d{9}')
+        app_code_pattern = re.compile(r'F\d{6}\sA')
 
         RC = '001'
+        card_type = 'RD'
+        rrn = gen_trans_id()
         const_fields = re.search(pattern, request)[0]
+        optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
+        app_code = re.search(app_code_pattern, request)[0]
 
-        response = const_fields + RC
+        try:
+            sequence_number = re.search(seq_pattern, request)[0] + '0'
+        except TypeError:
+            return reverse_answer()
+
+        response = '\x1c'.join([const_fields + RC, app_code, card_type, optional_data, sequence_number, 't' + rrn])
         response = response + b'\x03'.decode('utf-8')
         LRC = gen_check_val(response.encode('utf-8')[1:])
         response += LRC
@@ -72,11 +97,10 @@ def form_answer(request):
     def refund_answer():
         pattern = re.compile(r'\x02.{45}')
         seq_pattern = re.compile(r'h\d{9}')
-        rrn_pattern = re.compile(r't(\d{8})')
 
-        RC = '001'
+        RC = rc_dict['refund_rc']
         card_type = 'RD'
-        rrn = re.search(rrn_pattern, request).group(1)
+        rrn = gen_trans_id()
         const_fields = re.search(pattern, request)[0]
         approval_code = 'F' + ''.join([str(randint(0, 9)) for i in range(6)]) + ' A'
         optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
@@ -90,20 +114,10 @@ def form_answer(request):
 
     def reverse_answer():
         pattern = re.compile(r'\x02.{45}')
-        rrn_pattern = re.compile(r't(\d{8})')
 
-        try:
-            rrn = re.search(rrn_pattern, request).group(1)
-            optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
-        except TypeError:
-            rrn = ''.join([str(randint(0, 9)) for i in range(8)])
-            optional_data = f'a&C643#&R01#&r{rrn}#&p{rrn}#'
-
-        RC = '001'
+        RC = rc_dict['void_rc']
         const_fields = re.search(pattern, request)[0]
-
-        response = '\x1c'.join([const_fields + RC, optional_data, 't' + rrn])
-
+        response = ''.join([const_fields, RC])
         response = response + b'\x03'.decode('utf-8')
         LRC = gen_check_val(response.encode('utf-8')[1:])
         response += LRC
@@ -183,25 +197,42 @@ def conf_parser():
     if not os.path.exists('config.ini'):
         file = open('config.ini', 'w')
         file.write('[Srv_settings]\n'
-                   'SERVER_ADDRESS=\n'
+                   f'SERVER_ADDRESS={str(socket.gethostbyname_ex(socket.gethostname())[2][0])}\n'
                    'SERVER_PORT=\n'
-                   'HEX_DUMP=\n\n'
+                   'HEX_DUMP=0\n\n'
                    '[Ali_settings]\n'
-                   'RC=\n'
-                   'TIME=')
+                   'STATUS=1\n'
+                   'PROCESS_TIME=0\n'
+                   'REFUND_RC=001\n'
+                   'VOID_RC=001\n\n'
+                   '[DELAY]\n'
+                   'VOID_DELAY=0\n'
+                   'FINAL_DELAY=0\n'
+                   'STATUS_DELAY=0\n'
+                   'REFUND_DELAY=0\n'
+                   'QR_CODE_DELAY=0\n'
+                   'DEFAULT_DELAY=0\n'
+                   'CLOSE_OP_DELAY=0\n')
         file.close()
 
     conf = Config('config.ini')
     try:
         sett_dict = {
-            'addr': conf.as_args()[conf.as_args().index('--Srv-settings-SERVER-ADDRESS') + 1],
+            'hexd': conf.as_args()[conf.as_args().index('--Srv-settings-HEX-DUMP') + 1],
             'port': conf.as_args()[conf.as_args().index('--Srv-settings-SERVER-PORT') + 1],
-            'resp': conf.as_args()[conf.as_args().index('--Ali-settings-RC') + 1],
-            'time': conf.as_args()[conf.as_args().index('--Ali-settings-TIME') + 1],
-            'hexd': conf.as_args()[conf.as_args().index('--Srv-settings-HEX-DUMP') + 1]
+            'addr': conf.as_args()[conf.as_args().index('--Srv-settings-SERVER-ADDRESS') + 1],
+            'stat': conf.as_args()[conf.as_args().index('--Ali-settings-STATUS') + 1],
+            'stat_time': conf.as_args()[conf.as_args().index('--Ali-settings-PROCESS-TIME') + 1],
+            'refund_rc': conf.as_args()[conf.as_args().index('--Ali-settings-REFUND-RC') + 1],
+            'void_rc': conf.as_args()[conf.as_args().index('--Ali-settings-VOID-RC') + 1],
+            'void_time': conf.as_args()[conf.as_args().index('--DELAY-VOID-DELAY') + 1],
+            'final_time': conf.as_args()[conf.as_args().index('--DELAY-FINAL-DELAY') + 1],
+            'status_time': conf.as_args()[conf.as_args().index('--DELAY-STATUS-DELAY') + 1],
+            'refund_time': conf.as_args()[conf.as_args().index('--DELAY-REFUND-DELAY') + 1],
+            'qr_code_time': conf.as_args()[conf.as_args().index('--DELAY-QR-CODE-DELAY') + 1],
+            'default_time': conf.as_args()[conf.as_args().index('--DELAY-DEFAULT-DELAY') + 1],
+            'close_op_time': conf.as_args()[conf.as_args().index('--DELAY-CLOSE-OP-DELAY') + 1]
         }
-        if len(sett_dict['resp']) < 3:
-            sett_dict['resp'] = '0' * (3 - len(sett_dict['resp'])) + sett_dict['resp']
 
         set_logging(); logging.info(f'Config values:{sett_dict}')
         return sett_dict
--- server.py
+++ server.py
@@ -4,23 +4,64 @@ from select import select
 import resp_gen
 import version
 import logging
+import time
 import sys
 
 
 def setup_vars():
-    global SRV_ADDRESS
-    global INPUT_DATA
-    global OUTPUT_DATA
+    def get_rc():
+        global REFUND_RC
+        global VOID_RC
+
+        REFUND_RC = str(int(config_dict['refund_rc']))
+        VOID_RC = str(int(config_dict['void_rc']))
+        if len(REFUND_RC) != 3:
+            REFUND_RC = '0' * (3 - len(REFUND_RC)) + REFUND_RC
+        if len(VOID_RC) != 3:
+            VOID_RC = '0' * (3 - len(VOID_RC)) + VOID_RC
+
+    global TIMER
+    global RC_DICT
     global PRINT_HEX
+    global INPUT_DATA
+    global VOID_DELAY
+    global OUTPUT_DATA
+    global FINAL_DELAY
+    global SRV_ADDRESS
+    global PROCESS_TIME
     global LAST_REQUEST
+    global REFUND_DELAY
+    global STATUS_DELAY
+    global DEFAULT_DELAY
+    global QR_CODE_DELAY
+    global CLOSE_OP_DELAY
+    global TIMER_CONN_LIST
+    global STATUS_TIMER_LIST
 
     try:
-        config_dict = resp_gen.conf_parser()
+        config_dict = resp_gen.conf_parser()  # converts config.ini into python dict
+        get_rc()  # prepare response code for refund and void operations
+
         SRV_ADDRESS = (config_dict['addr'], int(config_dict['port']))
+        PROCESS_TIME = int(config_dict['stat_time'])
         PRINT_HEX = bool(int(config_dict['hexd']))
         INPUT_DATA = list()
         OUTPUT_DATA = list()
         LAST_REQUEST = dict()
+        TIMER_CONN_LIST = list()
+        STATUS_TIMER_LIST = list()
+
+        VOID_DELAY = int(config_dict['void_time'])
+        FINAL_DELAY = int(config_dict['final_time'])
+        STATUS_DELAY = int(config_dict['status_time'])
+        REFUND_DELAY = int(config_dict['refund_time'])
+        QR_CODE_DELAY = int(config_dict['qr_code_time'])
+        DEFAULT_DELAY = int(config_dict['default_time'])
+        CLOSE_OP_DELAY = int(config_dict['close_op_time'])
+
+        RC_DICT = {'sale_rc': bool(int(config_dict['stat'])),
+                   'refund_rc': REFUND_RC,
+                   'void_rc': VOID_RC}
     except ValueError:
         print('Wrong values in config.ini!')
         input('Press ENTER to exit.')
@@ -35,19 +76,89 @@ def get_server_socket():
     return server
 
 
+def ready_to_answer(addr, resp=None):
+    for conn in TIMER_CONN_LIST:
+        if addr in conn:
+            if time.perf_counter() > conn[1]:
+                TIMER_CONN_LIST.pop(TIMER_CONN_LIST.index(conn))
+                return True
+            else:
+                return False
+    else:
+        cur_time = time.perf_counter()
+        if len(resp) >= 49:
+            try:
+                if resp[41:43] == b'00':
+                    TIMER_CONN_LIST.append([addr, cur_time + VOID_DELAY])
+                elif resp[41:43] == b'01':
+                    TIMER_CONN_LIST.append([addr, cur_time + QR_CODE_DELAY])
+                elif resp[41:43] == b'02':
+                    TIMER_CONN_LIST.append([addr, cur_time + FINAL_DELAY])
+                elif resp[41:43] == b'04':
+                    TIMER_CONN_LIST.append([addr, cur_time + REFUND_DELAY])
+                elif resp[41:43] == b'36':
+                    TIMER_CONN_LIST.append([addr, cur_time + STATUS_DELAY])
+                else:
+                    TIMER_CONN_LIST.append([addr, cur_time + DEFAULT_DELAY])
+
+                for connect in TIMER_CONN_LIST:
+                    if addr in connect:
+                        if TIMER_CONN_LIST[TIMER_CONN_LIST.index(connect)][1] == cur_time:
+                            return True
+                        else:
+                            return False
+
+            except TypeError as e:
+                resp_gen.set_logging(); logging.error(f'ERROR:\n{e}')
+                return False
+        else:
+            if resp:
+                TIMER_CONN_LIST.append([addr, cur_time + CLOSE_OP_DELAY])
+            return False
+
+
+def status_ready(peer_name, request):
+    if request[41:43] == '36':
+        if PROCESS_TIME == 0:
+            return True
+        else:
+            cur_time = time.perf_counter()
+            for peer in STATUS_TIMER_LIST:
+                if peer_name in peer:
+                    if cur_time > peer[1]:
+                        # STATUS_TIMER_LIST.pop(STATUS_TIMER_LIST.index(peer))
+                        return True
+                    else:
+                        return False
+            else:
+                STATUS_TIMER_LIST.append([peer_name, cur_time + PROCESS_TIME])
+                return False
+    else:
+        return True
+
+
 def handle_writables(writs):
     for resource in writs:
         try:
-            resp = resp_gen.form_answer(LAST_REQUEST[resource.getpeername()].decode('utf-8'))
-            print(f'RESPONSE TO {resource.getpeername()}:\n{resp_gen.print_req_res(resp)}\n')
-            resp_gen.set_logging()
-            logging.info(f'RESPONSE TO  {resource.getpeername()}:{str(resp)}')
+            req = LAST_REQUEST[resource.getpeername()].decode('utf-8')
+            resp = resp_gen.form_answer(req,
+                                        RC_DICT,
+                                        status_ready(resource.getpeername(), req))
 
-            if PRINT_HEX:
-                print(resp_gen.print_hex_dump(resp))
-            if resp:
+            if resp and ready_to_answer(resource.getpeername(), resp):
+                print(f'RESPONSE TO {resource.getpeername()}:\n{resp_gen.print_req_res(resp)}\n')
+                resp_gen.set_logging()
+                logging.info(f'RESPONSE TO  {resource.getpeername()}:{str(resp)}')
+
+                if PRINT_HEX:
+                    print(resp_gen.print_hex_dump(resp))
                 resource.send(resp)
-            OUTPUT_DATA.remove(resource)
+                OUTPUT_DATA.remove(resource)
+
+                if resp.decode('utf-8').find('SUCCESS') != -1 or resp.decode('utf-8').find('FAILED') != -1:
+                    for peer in STATUS_TIMER_LIST:
+                        if resource.getpeername() in peer:
+                            STATUS_TIMER_LIST.pop(STATUS_TIMER_LIST.index(peer))
         except OSError:
             clear_resource(resource)
 
@@ -88,18 +199,23 @@ def handle_readables(reads, server):
 
 
 def clear_resource(res):
-    if res in INPUT_DATA:
-        INPUT_DATA.remove(res)
-    if res in OUTPUT_DATA:
-        OUTPUT_DATA.remove(res)
-    print(f'Connection closed:{str(res.getpeername())}\n')
-    resp_gen.set_logging(); logging.info(f'Connection closed:{str(res)}')
-    res.close()
+    try:
+        if res in INPUT_DATA:
+            INPUT_DATA.remove(res)
+        if res in OUTPUT_DATA:
+            OUTPUT_DATA.remove(res)
+        print(f'Connection closed:{str(res.getpeername())}\n')
+        resp_gen.set_logging(); logging.info(f'Connection closed:{str(res)}')
+        res.close()
+    except Exception as e:
+        resp_gen.set_logging()
+        logging.info(f'ERROR:\n{e}')
 
 
 if __name__ == '__main__':
-    setup_vars()
-    resp_gen.check_log_files()
+    setup_vars()  # initiates variables
+    time.perf_counter()  # initiates timer
+    resp_gen.check_log_files()  # controls maximal log files
 
     try:
         srv_socket = get_server_socket()
